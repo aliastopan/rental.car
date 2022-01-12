ec28145fd516c5d78f5b5e85deb55257e74a2f5e
= <taufan.augusta@protonmail.com>
2022-01-13 04:28:13 +0700

    test: wwwroot image upload

--- src/Infrastructure/Services/FileManager.cs
+++ src/Infrastructure/Services/FileManager.cs
@@ -1,13 +1,23 @@
 using System.IO;
-using  Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Hosting;
 using Infrastructure.Common;
 
 namespace Infrastructure.Services
 {
     public class FileManager
     {
-        // to do :
-        // check if exist or add GUID to file name
+        private IWebHostEnvironment? env { get; init; }
+
+        public FileManager(IWebHostEnvironment env)
+        {
+            this.env = env;
+        }
+
+        public string GetRootDirectory(string subfolder)
+        {
+            return $"{env!.WebRootPath}\\{subfolder}";
+        }
 
         public string GetDataStoragePath(string subfolder)
         {
@@ -20,15 +30,9 @@ namespace Infrastructure.Services
 
         public string GetFilePath(IBrowserFile file, string subfolder)
         {
-            string directoryPath = GetDataStoragePath(subfolder);
+            string directoryPath = GetRootDirectory(subfolder);
             return Path.GetFullPath(Path.Combine(directoryPath, file.Name));
         }
-
-        // private string Rename(string subfolder)
-        // {
-        //     string guid = Guid.NewGuid().ShortenGuid();
-
-        // }
 
         public async Task UploadAsync(IBrowserFile file, string subfolder)
         {
@@ -43,36 +47,6 @@ namespace Infrastructure.Services
 
         }
 
-        public async Task UploadAsync(IBrowserFile file, string subfolder, string rename)
-        {
-            string filePath = GetFilePath(file, subfolder);
-            Stream stream = file.OpenReadStream();
-            FileStream fileStream = File.Create(filePath);
-
-            await stream.CopyToAsync(fileStream);
-
-            stream.Close();
-            fileStream.Close();
-
-            string directory = GetDataStoragePath(subfolder);
-            string renamePath = Path.GetFullPath(Path.Combine(directory, rename));
-            File.Move(filePath, renamePath);
-
-        }
-
-        public string RenameFile(IBrowserFile file, string subfolder)
-        {
-            string filePath = GetFilePath(file, subfolder);
-            string directory = GetDataStoragePath(subfolder);
-            string guid = Guid.NewGuid().ShortenGuid();
-            string extension = Path.GetExtension(filePath);
-            string rename = Path.GetFullPath(Path.Combine(directory, $"{guid}{extension}"));
-
-            File.Move(filePath, rename);
-
-            return rename;
-        }
-
         public void Upload(IBrowserFile file, string subfolder)
         {
             string filePath = GetFilePath(file, subfolder);
@@ -83,6 +57,19 @@ namespace Infrastructure.Services
 
             stream.Close();
             fileStream.Close();
+        }
+
+        public string RenameFile(IBrowserFile file, string subfolder)
+        {
+            string filePath = GetFilePath(file, subfolder);
+            string directory = GetRootDirectory(subfolder);
+            string guid = Guid.NewGuid().ShortenGuid();
+            string extension = Path.GetExtension(filePath);
+            string rename = Path.GetFullPath(Path.Combine(directory, $"{guid}{extension}"));
+
+            File.Move(filePath, rename);
+
+            return rename;
         }
     }
 }
